fd60f057aceff95a65ac43c71834dd561e7735d4
GowthamSaas <gowtham@saastrail.com>
2026-06-30 14:49:54 +0530

    update the main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
+
+
+
 // import { createApp } from 'vue'
 // import { createPinia } from 'pinia'
 // import PrimeVue from 'primevue/config'
 // import ToastService from 'primevue/toastservice'
+// import Aura from '@primeuix/themes/aura'
 // import ConfirmationService from 'primevue/confirmationservice'
 // import Tooltip from 'primevue/tooltip'
 
 // import App from './App.vue'
 // import router from './router'
+// import { useAuthStore } from './stores/auth'
 
 // // PrimeVue styles
 // import 'primevue/resources/themes/lara-light-blue/theme.css'
@@ -14,22 +19,42 @@
 // import 'primeicons/primeicons.css'
 // import './assets/main.css'
 
-// const app = createApp(App)
+// async function bootstrap() {
+//   const app = createApp(App)
 
-// app.use(createPinia())
-// app.use(router)
-// app.use(PrimeVue)
-// app.use(ToastService)
-// app.use(ConfirmationService)
-// app.directive('tooltip', Tooltip)
+//   const pinia = createPinia()
+//   app.use(pinia)
 
-// app.mount('#app')
+//   // Initialize auth before router
+//   const authStore = useAuthStore()
+//   if (authStore.initializeAuth) {
+//     await authStore.initializeAuth()
+//   }
+
+//   app.use(PrimeVue, {
+//     theme: {
+//         preset: Aura
+//     }
+// })
+
+//   app.use(router)
+//   app.use(ToastService)
+//   app.use(ConfirmationService)
+//   app.directive('tooltip', Tooltip)
+
+//   app.mount('#app')
+// }
+
+// bootstrap()
 
 
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
 import ToastService from 'primevue/toastservice'
 import ConfirmationService from 'primevue/confirmationservice'
 import Tooltip from 'primevue/tooltip'
@@ -38,9 +63,6 @@ import App from './App.vue'
 import router from './router'
 import { useAuthStore } from './stores/auth'
 
-// PrimeVue styles
-import 'primevue/resources/themes/lara-light-blue/theme.css'
-import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 import './assets/main.css'
 
@@ -56,10 +78,16 @@ async function bootstrap() {
     await authStore.initializeAuth()
   }
 
+  app.use(PrimeVue, {
+    theme: {
+      preset: Aura
+    }
+  })
+
   app.use(router)
-  app.use(PrimeVue)
   app.use(ToastService)
   app.use(ConfirmationService)
+
   app.directive('tooltip', Tooltip)
 
   app.mount('#app')
