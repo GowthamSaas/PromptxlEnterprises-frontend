64141c5aa781af463bcb9d2ca6fc6c3294bd19fc
GowthamSaas <gowtham@saastrail.com>
2026-07-08 18:45:58 +0530

    monocco editor changes

--- src/components/ai_workspace/FileContextMenu.vue
+++ src/components/ai_workspace/FileContextMenu.vue
@@ -1,12 +1,12 @@
 <template>
   <ContextMenu
     ref="menu"
-    :model="items"
+    :model="computedItems"
   />
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 import ContextMenu from "primevue/contextmenu";
 
@@ -19,42 +19,83 @@ const emit = defineEmits([
 
 const menu = ref();
 
-const items = [
+const nodeType = ref("folder"); // 'folder', 'file', or 'root'
 
+const baseItems = [
   {
     label: "New File",
     icon: "pi pi-file",
     command: () => emit("new-file"),
   },
-
   {
     label: "New Folder",
     icon: "pi pi-folder",
     command: () => emit("new-folder"),
   },
-
   {
     separator: true,
   },
-
   {
     label: "Rename",
     icon: "pi pi-pencil",
     command: () => emit("rename"),
   },
-
   {
     label: "Delete",
     icon: "pi pi-trash",
     command: () => emit("delete"),
   },
-
 ];
 
-function show(event) {
+const folderItems = [
+  {
+    label: "New File",
+    icon: "pi pi-file",
+    command: () => emit("new-file"),
+  },
+  {
+    label: "New Folder",
+    icon: "pi pi-folder",
+    command: () => emit("new-folder"),
+  },
+  {
+    separator: true,
+  },
+  {
+    label: "Rename",
+    icon: "pi pi-pencil",
+    command: () => emit("rename"),
+  },
+  {
+    label: "Delete",
+    icon: "pi pi-trash",
+    command: () => emit("delete"),
+  },
+];
 
+const fileItems = [
+  {
+    label: "Rename",
+    icon: "pi pi-pencil",
+    command: () => emit("rename"),
+  },
+  {
+    label: "Delete",
+    icon: "pi pi-trash",
+    command: () => emit("delete"),
+  },
+];
+
+const computedItems = computed(() => {
+  if (nodeType.value === "file") {
+    return fileItems;
+  }
+  return folderItems;
+});
+
+function show(event, type = "folder") {
+  nodeType.value = type;
   menu.value.show(event);
-
 }
 
 defineExpose({
